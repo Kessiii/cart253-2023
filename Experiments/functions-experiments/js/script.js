50d369255e03a8f55106fc6b2c6d295a01ef76ee
Kessiii <kesvi@icloud.com>
2023-10-05 09:47:18 -0400

    PArameters
    Making the code effficient!

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -31,21 +31,21 @@ function setup() {
 function draw() {
     background(0);
 
-   parallels(100,100,5,1,100);
-   parallels(50,50,10,2,20);
-   parallels(200,200,15,7,3);
-   parallels(312,257,20,0.5,300);
+   parallels(100,100,5,1,100,1);
+   parallels(50,50,10,2,20,10);
+   parallels(200,200,15,7,3,20);
+   parallels(312,257,20,0.5,300,1);
 
 }
 
-function parallels(x,y,numLines,lineWidth,lineHeight) {
+function parallels(x,y,numLines,lineWidth,lineHeight,lineSpacing) {
     
     for (let i = 0; i < numLines; i++) {
         noStroke();
         fill(255);
         rectMode(CENTER);
         rect(x,y,lineWidth,lineHeight);
-        x = x + 10;
+        x = x + lineSpacing;
 
     }
 }
