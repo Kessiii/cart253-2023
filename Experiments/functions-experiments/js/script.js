992491a16af49b794f9fc5f15bf75470bd699ab8
Kessiii <kesvi@icloud.com>
2023-10-05 00:44:17 -0400

    Creating a new functions from scratch

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -19,8 +19,16 @@ function preload() {
 /**
  * Description of setup
 */
-function setup() {
+let circle = {
+    x:0,
+    y:250,
+    size:100,
+    vx: 1,
+    vy:0
+}
 
+function setup() {
+    createCanvas(500,500);
 }
 
 
@@ -28,5 +36,23 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy; 
+
+    if (circle.x > width) {
+        circle.x = 0;
+        circle.vx = circle.vx + 2;
+        circle.size = circle.size + 5;
+    }
+
+    fill(255,0,0)
+    ellipse(circle.x,circle.y,circle.size);
+}
+
+function mousePressed() {
+    circle.x = 0;
+    circle.vx = circle.vx + 2;
+    circle.size + circle.size + 5;
 }
