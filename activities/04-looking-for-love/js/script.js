4875b2edff842f0d317e01d7716d5b171366659a
Kessiii <kesvi@icloud.com>
2023-10-05 15:54:11 -0400

    Added a title and MousePress function

--- activities/04-looking-for-love/js/script.js
+++ activities/04-looking-for-love/js/script.js
@@ -28,8 +28,88 @@ let circle2 = {
 };
 
 
+let state = `title`; // Can be: title, simulation, love, sadness
+
 function setup() {
     createCanvas(500,500);
+
+    setupCircles();
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+    background(0);
+
+    if (state === `title`) {
+        title();
+    }
+    else if (state ===`simulation`) {
+        simulation();
+    }
+    else if (state=== `love`) {
+
+    }
+    else if (state === `sadness`) {
+
+    }
+
+}
+
+function title() {
+    textSize(64);
+    fill(200,100,100);
+    textAlign(CENTER,CENTER);
+    text(`LOVE?`,width/2,height/2);
+
+}
+
+function simulation() {
+   
+    move();
+    checkOffscreen();
+    checkOverlap();
+    display();
+
+}
+
+
+function move() {
+    //Move the circles
+    circle1.x = circle1.x + circle1.vx;
+    circle1.y = circle1.y + circle1.vy;
+
+    circle2.x = circle2.x + circle2.vx;
+    circle2.y = circle2.y + circle2.vy;
+
+}
+
+function checkOffscreen() {
+
+     //Check if the circles have gone offscreen
+     if(circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height) {
+        //SAD ending
+    }
+}
+
+function checkOverlap() {
+
+       //Check if the circles overlap
+       let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
+       if(d < circle1.size/2 + circle2.size/2) {
+           //LOVE ENDING
+       }
+}
+
+function display() {
+      //Diplaying the Circles
+      ellipse(circle1.x,circle1.y,circle1.size);
+      ellipse(circle2.x,circle2.y,circle2.size);
+}
+
+function setupCircles() {
 
     //Postion circles seperated from one another
     circle1.x = width/3;
@@ -42,26 +122,8 @@ function setup() {
     circle2.vy = random(-circle2.speed,circle2.speed);
 }
 
-
-/**
- * Description of draw()
-*/
-function draw() {
-    background(0);
-
-    //Moving the Circles
-    circle1.x = circle1.x + circle1.vx;
-    circle1.y = circle1.y + circle1.vy;
-
-    circle2.x = circle2.x + circle2.vx;
-    circle2.y = circle2.y + circle2.vy;
-
-    //Check if the circles have gone offscreen
-    if(circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height) {
-        //SAD ending
+function mousePressed() {
+    if (state === `title`) {
+        state = `simulation`;
     }
-
-    //Diplaying the Circles
-    ellipse(circle1.x,circle1.y,circle1.size);
-    ellipse(circle2.x,circle2.y,circle2.size);
 }
