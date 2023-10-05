ce9e064ebfc71dbf1a1df4154fb1c7f0f9304b87
Kessiii <kesvi@icloud.com>
2023-10-05 11:25:25 -0400

    Tried Text
    Testing how to show words using text and strings!

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -19,14 +19,19 @@ function preload() {
 /**
  * Description of setup
 */
+
+let hello = {
+    string: `Hello, world!`,
+    x: 0,
+    y: 0,
+    vx: 5,
+    vy: 1,
+    size: 64
+};
+
 function setup() {
     createCanvas(500,500);
 
-    let hotCelsius = toCelsius(100);
-    console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`);
-
-    let coldCelsius = toCelsius(10);
-    console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`);
 }
 
 
@@ -37,9 +42,19 @@ function setup() {
 function draw() {
     background(0);
 
-}
+    hello.x = hello.x + hello.vx;
+    hello.y = hello.y + hello.vy;
 
-function toCelsius(fahrenheit) {
-    let celsius = (fahrenheit - 32) * 5/9;
-    return celsius;
+    hello.size = hello.size + 1; 
+
+    textAlign(CENTER,CENTER)
+    textSize(hello.size)
+    textStyle(BOLD);
+
+    fill(200,50,200);
+    stroke(50,200,50);
+    strokeWeight(3);
+
+    text(hello.string,hello.x,hello.y);
+
 }
