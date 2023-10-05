f08d382f06511802462e9ac3b804da6c0e3b2611
Kessiii <kesvi@icloud.com>
2023-10-05 09:42:29 -0400

    Adding more parameters

--- Experiments/functions-experiments/js/script.js
+++ Experiments/functions-experiments/js/script.js
@@ -27,10 +27,26 @@ function setup() {
 /**
  * Description of draw()
 */
+
 function draw() {
     background(0);
 
+   parallels(100,100,5,1,100);
+   parallels(50,50,10,2,20);
+   parallels(200,200,15,7,3);
+   parallels(312,257,20,0.5,300);
 
 }
 
+function parallels(x,y,numLines,lineWidth,lineHeight) {
+    
+    for (let i = 0; i < numLines; i++) {
+        noStroke();
+        fill(255);
+        rectMode(CENTER);
+        rect(x,y,lineWidth,lineHeight);
+        x = x + 10;
+
+    }
+}
 
