06851efdc356ba1474a3506407a93e707f856f14
Kessiii <kesvi@icloud.com>
2023-10-12 09:27:37 -0400

    Starting the Simulator with a new template

--- Exercises/Project 1 - Simulator/js/script.js
+++ Exercises/Project 1 - Simulator/js/script.js
@@ -16,20 +16,11 @@ function preload() {
 }
 
 
-
 /**
  * Description of setup
 */
-
-let p = [];
-const num = 100;
-
 function setup() {
-    createCanvas(400,400);
-    for(let i = 0; i < num; i ++) {
-        particles.push(createVector(random(width), random(height)))
-    }
-    stroke(255);
+
 }
 
 
@@ -37,9 +28,5 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(255);
-    for(let i = 0; i < num; i ++) {
-        let p = particles[i];
-        point(p.x, p.y)
-    }
+
 }
