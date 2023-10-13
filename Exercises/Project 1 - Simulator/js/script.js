5fb50edd8619c27d34f056e8bcc7b4d8114a095f
Kessiii <kesvi@icloud.com>
2023-10-12 21:58:11 -0400

    Setting up the flow fields

--- Exercises/Project 1 - Simulator/js/script.js
+++ Exercises/Project 1 - Simulator/js/script.js
@@ -18,15 +18,17 @@ function preload() {
 /**
  * Description of setup
 */
-let p = [];
+let particles = [];
 const num = 100;
 
 function setup() {
     createCanvas(400, 400);
     for(let i = 0; i < num; i ++) {
-        p.push(createVector(random(width), random(height)));
+        particles.push(createVector(random(width), random(height)));
     }
+    stroke(255);
 }
+
 
 
 /**
@@ -34,4 +36,8 @@ function setup() {
 */
 function draw() {
     background(0)
+    for(let i = 0; i < num; i ++) {
+        let p = particles[i];
+        point(p.x, p.y);
+    }
 }
