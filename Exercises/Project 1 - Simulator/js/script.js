a2d8ed4da2bb724a7f7afc8f02873bc1430f9a6b
Kessiii <kesvi@icloud.com>
2023-10-19 00:18:59 -0400

    Different Approach
    I am trying a different idea by making the particles follow the cursor, I think I am getting the actions right once the mouse is clicked, just the movement is not following the cursor

--- Exercises/Project 1 - Simulator/js/script.js
+++ Exercises/Project 1 - Simulator/js/script.js
@@ -21,20 +21,21 @@ function preload() {
 let particles = [];
 const num = 10000;
 const noiseScale = 0.033;
+let followCursor = false; //added a boolean to control particle movement
 
-let bgColor;
-let strokeColor;
-let colorInverted = false; // added boolean for color inversion
+//let bgColor;
+//let strokeColor;
+//let colorInverted = false; // added boolean for color inversion
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    bgColor = color(255); //Initial background color
-    strokeColor = color(169, 144, 117); // Initial stroke color
+    //bgColor = color(255); //Initial background color
+    //strokeColor = color(169, 144, 117); // Initial stroke color
 
     for(let i = 0; i < num; i ++) {
         particles.push(createVector(random(width), random(height)));
     }
-    stroke(strokeColor);
+    stroke(255);
 }
 
 
@@ -46,25 +47,33 @@ function setup() {
 let cursorX, cursorY;
 
 function draw() {
-    cursorX = mouseX; // Store the curent X position of the cursor
-    cursorY = mouseY; // Sotre the current Y position of the cursor
-
-    if (colorInverted) {
-        background(169, 144, 117, 10); //inverted background color
-        stroke(255);
-    } else {
-        background(bgColor); //Original background color
-        stroke(strokeColor); //Original stroke color
+    if (followCursor) {
+        cursorX = mouseX; // Store the curent X position of the cursor
+        cursorY = mouseY; // Sotre the current Y position of the cursor
     }
+    
+    background(169, 144, 117, 10);
+
+    //if (colorInverted) {
+        //background(169, 144, 117, 10); //inverted background color
+        //stroke(255);
+    //} else {
+        //background(bgColor); //Original background color
+        //stroke(strokeColor); //Original stroke color
+    //}
 
 
     for(let i = 0; i < num; i ++) {
         let p = particles[i];
         point(p.x, p.y);
-        let n = noise((p.x + cursorX) * noiseScale, (p.y + cursorY) * noiseScale);
-        let a = TAU * n;
-        p.x += cos(a);
-        p.y += sin(a);
+
+        if (followCursor) {
+            let n = noise((p.x + cursorX) * noiseScale, (p.y + cursorY) * noiseScale);
+            let a = TAU * n;
+            p.x += cos(a);
+            p.y += sin(a);
+        }
+
         if(!onScreen(p)) {
             p.x = random(width);
             p.y = random(height);
@@ -77,11 +86,15 @@ function draw() {
     //noiseSeed(millis());
 //}
 
-function mousePressed() {
-    //Toggle the color inversion state when the mouse is clicked
-    colorInverted = !colorInverted;
+function onScreen(v) {
+    return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
 }
 
-function onScreen(v) {
-    return v.x >= 0 && v.x >= 0 && v.y <= height;
+function mousePressed() {
+    //Toggle the color inversion state when the mouse is clicked
+    followCursor = true;
+}
+
+function mouseReleased() {
+    followCursor = false;
 }
