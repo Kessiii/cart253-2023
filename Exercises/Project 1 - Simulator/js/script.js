320d810406b0434626c481e2ae7071f6bf1b35ee
Kessiii <kesvi@icloud.com>
2023-10-19 10:25:39 -0400

    Brought back mouse pressed
    I brought back the code to react when mousePressed, I really like the effect.

--- Exercises/Project 1 - Simulator/js/script.js
+++ Exercises/Project 1 - Simulator/js/script.js
@@ -21,39 +21,63 @@ function preload() {
 let particles = [];
 const num = 10000;
 const noiseScale = 0.033;
+let followCursor = false;
 
 function setup() {
-    createCanvas(1000, 800);
+    createCanvas(windowWidth, windowHeight);
+    stroke(255);
+    resetParticles();
+}
+
+function resetParticles() {
+    particles = []; //Clear the current particles
     for(let i = 0; i < num; i ++) {
         particles.push(createVector(random(width), random(height)));
     }
-    stroke(255);
 }
-
-
 
 /**
  * Description of draw()
 */
 
-let cursorX, cursorY;
+//let cursorX, cursorY;
 
 function draw() {
-    cursorX = mouseX; // Store the curent X position of the cursor
-    cursorY = mouseY; // Sotre the current Y position of the cursor
+    //cursorX = mouseX; // Store the curent X position of the cursor
+    //cursorY = mouseY; // Sotre the current Y position of the cursor
 
     background(169, 144, 117, 10);
+
     for(let i = 0; i < num; i ++) {
         let p = particles[i];
-        point(p.x, p.y);
-        let n = noise((p.x + cursorX) * noiseScale, (p.y + cursorY) * noiseScale);
-        let a = TAU * n;
-        p.x += cos(a);
-        p.y += sin(a);
-        if(!onScreen(p)) {
-            p.x = random(width);
-            p.y = random(height);
+
+        if (followCursor) {
+            let targetX = mouseX + random(-10, 10);
+            let targetY = mouseY + random(-10, 10);
+            p.x = lerp(p.x, targetX, 0.1);
+            p.y = lerp(p.y, targetY, 0.1);
+        } else {
+            let n = noise(p.x * noiseScale, p.y * noiseScale);
+            let a = TAU * n;
+            p.x += cos(a);
+            p.y += sin(a);
         }
+
+        //Wrap particles around the canvas
+        p.x = (p.x + width) % width;
+        p.y = (p.y + height) % height;
+
+        ellipse(p.x, p.y, 1, 1);
+
+        //point(p.x, p.y);
+        //let n = noise((p.x + cursorX) * noiseScale, (p.y + cursorY) * noiseScale);
+        //let a = TAU * n;
+        //p.x += cos(a);
+        //p.y += sin(a);
+        //if(!onScreen(p)) {
+            //p.x = random(width);
+            //p.y = random(height);
+       // }
     }
     
 }
@@ -62,6 +86,13 @@ function draw() {
     //noiseSeed(millis());
 //}
 
-function onScreen(v) {
-    return v.x >= 0 && v.x >= 0 && v.y<= height;
+//function onScreen(v) {
+    //return v.x >= 0 && v.x >= 0 && v.y<= height;
+//}
+
+function mousePressed() {
+    followCursor = !followCursor;
+    if (!followCursor) {
+        resetParticles();
+    }
 }
