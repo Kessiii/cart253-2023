781125fa7168ce16272f724cf6ae4ab980888aed
Kessiii <kesvi@icloud.com>
2023-10-19 02:11:59 -0400

    RESTARTING
    I am loosing it... restarting from where I was, I liked the flow field and the movement, I will try again but I will be a bit more organized....

--- Exercises/Project 1 - Simulator/js/script.js
+++ Exercises/Project 1 - Simulator/js/script.js
@@ -19,39 +19,16 @@ function preload() {
  * Description of setup
 */
 let particles = [];
-const num = 1000;
-const noiseScale = 0.005;
-let followCursor = false; //added a boolean to control particle movement
-let titleScreen = true;
-let enterButton;
-
-//let bgColor;
-//let strokeColor;
-//let colorInverted = false; // added boolean for color inversion
+const num = 10000;
+const noiseScale = 0.033;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    //bgColor = color(255); //Initial background color
-    //strokeColor = color(169, 144, 117); // Initial stroke color
-
-    noStroke();
-    resetParticles();
-
-    //Creating the "Enter" button
-    enterButton = createButton('Enter');
-    enterButton.position(width / 2 - 40, height / 2);
-    enterButton.mousePressed(enterFlowField);
-    enterButton.style('background-color', 'brown');
-    enterButton.mousePressed(enterFlowField)
-}
-
-function resetParticles() {
-    particles = []
+    createCanvas(1000, 800);
     for(let i = 0; i < num; i ++) {
         particles.push(createVector(random(width), random(height)));
     }
+    stroke(255);
 }
-
 
 
 
@@ -59,72 +36,32 @@ function resetParticles() {
  * Description of draw()
 */
 
-//let cursorX, cursorY;
+let cursorX, cursorY;
 
 function draw() {
-    if (titleScreen) {
-        background(169, 144, 117, 10);
-    } else {
+    cursorX = mouseX; // Store the curent X position of the cursor
+    cursorY = mouseY; // Sotre the current Y position of the cursor
 
-        followCursor = !followCursor;
-        for(let i = 0; i < num; i ++) {
-            let p = particles[i];
-
-            if (followCursor) {
-                let targetX = mouseX + random(-10, 10);
-                let targetY = mouseY + random(-10, 10);
-                p.x = lerp(p.x, targetX, 0.1);
-                p.y = lerp(p.y, targetY, 0.1);
-            } else {
-                let n = noise(p.x * noiseScale, p.y * noiseScale);
-                let a = TAU * n;
-                p.x += cos(a);
-                p.y += sin(a);
-            }
-
-            p.x = (p.x + width) % width;
-            p.y = (p.y + height) % height;
-    
-            fill(255, 100)
-            ellipse(p.x, p.y, 5, 5);
-           }
+    background(169, 144, 117, 10);
+    for(let i = 0; i < num; i ++) {
+        let p = particles[i];
+        point(p.x, p.y);
+        let n = noise((p.x + cursorX) * noiseScale, (p.y + cursorY) * noiseScale);
+        let a = TAU * n;
+        p.x += cos(a);
+        p.y += sin(a);
+        if(!onScreen(p)) {
+            p.x = random(width);
+            p.y = random(height);
         }
     }
-
     
-
-    //if (colorInverted) {
-        //background(169, 144, 117, 10); //inverted background color
-        //stroke(255);
-    //} else {
-        //background(bgColor); //Original background color
-        //stroke(strokeColor); //Original stroke color
-    //}
-    
-//}
+}
 
 //function mouseReleased() {
     //noiseSeed(millis());
 //}
 
-//function onScreen(v) {
-    //return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
-//}
-
-function enterFlowField() {
-    titleScreen = false;
-    followCursor = true;
-    enterButton.remove();
-}
-
-function mousePressed() {
-
-    if (!titleScreen) {
-        followCursor = !followCursor;
-    }
-   
-
-    if(!followCursor) {
-        resetParticles();
-    }
+function onScreen(v) {
+    return v.x >= 0 && v.x >= 0 && v.y<= height;
 }
