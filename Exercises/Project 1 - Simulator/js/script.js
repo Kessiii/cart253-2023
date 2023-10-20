457c45a31e1dfa285377d5582c8adb9b296776a7
Kessiii <kesvi@icloud.com>
2023-10-19 23:51:18 -0400

    End screen
    FINALLY got the end screen to appear once the song finishes. The Camel is satisfied...

--- Exercises/Project 1 - Simulator/js/script.js
+++ Exercises/Project 1 - Simulator/js/script.js
@@ -12,15 +12,19 @@
 */
 
 let cursorImage; //Declaring a variable to store the image
+var song;
 
 function preload() {
     cursorImage = loadImage('assets/images/cute-camel.png'); //loading my image
+    song = loadSound("Nascence.mp3");
 }
 
 
 /**
  * Description of setup
 */
+let state = `title`; //setting up the welcome screen
+
 let particles = [];
 const num = 15000;
 let noiseScale = 0.003;
@@ -38,13 +42,27 @@ function setup() {
     }
     noStroke();
     noCursor();
-    
+    song.play();
+    song.onended(songEnded); // Call songEnded function when the song ends
 }
 
 function draw() {
     background(169, 144, 117, 10);
+
+    if (state === `title`) {
+        title();
+    }
+    else if (state ===`simulation`) {
+        simulation();
+    }
+    else if (state === "finished" && !song.isPlaying()) {
+        finished();
+        }
+}
+
+
+function simulation() {
     image(cursorImage, mouseX, mouseY, 90, 90);
-    
 
     for (let i = 0; i < num; i++) {
         let p = particles[i];
@@ -88,8 +106,42 @@ function draw() {
 
 }
 
+function songEnded() {
+    state = "finished";
+}
+
+function title() {
+    push();
+    textSize(35);
+    fill(0);
+    textAlign(CENTER,CENTER);
+    text(`You are the One Heavenly Camel. 
+    You descend to this plane, 
+    bringing with you the continuous flow of the four winds. 
+    Move and the winds will move with you. 
+    Move until the end of your wind's song!
+    Click and Enter...`,width/2,height/2);
+    pop();
+}
+
+function finished() {
+    push();
+    textSize(35);
+    fill(0);
+    textAlign(CENTER,CENTER);
+    text(`Till next time Camel...
+    Till next time...`,width/2,height/2);
+    pop();
+}
 
 function mousePressed() {
+    if (state === "title") {
+      state = "simulation";
+    } else if (state === "finished") {
+      state = "title";
+      song.play(); // Start playing the song again
+    }
+
     followCursor = !followCursor;
     if (!followCursor) {
         targetNoiseScale = 0.08;
