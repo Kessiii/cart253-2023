cbf3b7aa13c86f3471b7fe1f2eff7a29aab1d0fa
Kessiii <kesvi@icloud.com>
2023-10-26 15:25:18 -0400

    Doing the Code
    Starting the code from the text on moodle!

--- Exercises/age-of-aquariums/js/script.js
+++ Exercises/age-of-aquariums/js/script.js
@@ -8,6 +8,7 @@
 
 "use strict";
 
+
 /**
  * Description of preload
 */
@@ -15,18 +16,81 @@ function preload() {
 
 }
 
+//let us set up our fishes!
+let fish1;
+let fish2;
+let fish3;
+let fish4;
 
 /**
  * Description of setup
 */
 function setup() {
+  createCanvas(600, 600);
 
+  //Time to position the fishes anywhere
+  fish1 = createFish(random(0, width), random(0, height));
+  fish2 = createFish(random(0, width), random(0, height));
+  fish3 = createFish(random(0, width), random(0, height));
+  fish4 = createFish(random(0, width), random(0, height));
+}
+
+//CreateFish(x, y)
+//Creating a new Javascript Object describing a fish and returns it
+function createFish(x, y) {
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2
+  };
+  return fish;
 }
 
 
 /**
- * Description of draw()
+ * Moves and Display the fishes & draws the background
 */
 function draw() {
+  background (0);
 
+  moveFish(fish1);
+  moveFish(fish2);
+  moveFish(fish3);
+  moveFish(fish4);
+
+  displayFish(fish1);
+  displayFish(fish2);
+  displayFish(fish3);
+  displayFish(fish4);
+}
+
+//moveFish(fish)
+//This chooses whether the provided fish changes direction and moves it
+function moveFish(fish) {
+  let change = random(0, 1);
+  if (change < 0.5) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
+
+  //Moving the fishes
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  //Constrain the fish to the Canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
+}
+
+//displayFish(fish)
+//Displays the provided fish on the canvas
+function displayFish(fish) {
+  push();
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
+  pop();
 }
