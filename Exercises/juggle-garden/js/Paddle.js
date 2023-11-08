12ddb5e163b772097682c0b4d68d518a60a8bc02
Kessiii <kesvi@icloud.com>
2023-11-08 00:46:26 -0500

    Changing the user control
    Changed the code to make the paddle controlled by the key A and D

--- Exercises/juggle-garden/js/Paddle.js
+++ Exercises/juggle-garden/js/Paddle.js
@@ -2,13 +2,23 @@ class Paddle {
     constructor(w,h) {
         this.width = w;
         this.height = h;
-        this.x = 0;
-        this.y = height - this.height/2;
-    }
+        this.x = width / 2;  // Start the paddle in the center of the canvas
+        this.y = height - this.height /2;
+        this.speed = 5;  // Adjust the speed as needed
+    }    
 
     move() {
-        this.x = mouseX;
+        if (keyIsDown(65)) {  // Check if 'A' key is pressed (ASCII code for 'A' is 65)
+            this.x -= this.speed;
+        }
+        if (keyIsDown(68)) {  // Check if 'D' key is pressed (ASCII code for 'D' is 68)
+            this.x += this.speed;
+        }
+
+        // Limit the paddle's x position to stay within the canvas width
+        this.x = constrain(this.x, this.width / 2, width - this.width / 2);
     }
+    
 
     display() {
         push();
