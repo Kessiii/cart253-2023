c905c7ec60d78825e1ce85c9871715b8fbf305ba
Kessiii <kesvi@icloud.com>
2023-11-08 01:19:19 -0500

    Changing Paddle
    Trying to fix and change the picture of the paddle

--- Exercises/juggle-garden/js/script.js
+++ Exercises/juggle-garden/js/script.js
@@ -11,49 +11,42 @@
 /**
  * Description of preload
 */
-function preload() {
-
-}
-
 let gravityForce = 0.0025;
-let balls = []
+let balls = [];
 let numBalls = 10;
 let paddle;
+let paddleImage; // Variable to hold the custom paddle image
 
-/**
- * Description of setup
-*/
-function setup() {
-
-    createCanvas(windowWidth, windowHeight);
-    paddle = new Paddle(300,20);
-
-    for (let i = 0; i < numBalls; i++) {
-        let x = random(0,width);
-        let y = random(-400,-100);
-        let ball = new Ball(x,y);
-        balls.push(ball);
-    }
-
+function preload() {
+    // Load the custom paddle image
+    paddleImage = loadImage('assets/images/running.png');
 }
 
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    paddle = new Paddle(paddleImage);
 
-/**
- * Description of draw()
-*/
+    for (let i = 0; i < numBalls; i++) {
+        let x = random(0, width);
+        let y = random(-400, -100);
+        let ball = new Ball(x, y);
+        balls.push(ball);
+    }
+}
+
 function draw() {
     background(0);
 
     paddle.move();
     paddle.display();
-    
+
     for (let i = 0; i < balls.length; i++) {
-        let ball = balls [i];
+        let ball = balls[i];
         if (ball.active) {
-        ball.gravity(gravityForce);
-        ball.move();
-        ball.bounce(paddle);
-        ball.display();
+            ball.gravity(gravityForce);
+            ball.move();
+            ball.bounce(paddle);
+            ball.display();
         }
     }
 }
