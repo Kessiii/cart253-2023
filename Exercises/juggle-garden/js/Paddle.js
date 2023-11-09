056c27cc319e140b42c9045d02375cec95f3b821
Kessiii <kesvi@icloud.com>
2023-11-09 00:49:37 -0500

    Struggling to get it to work
    Having a hard time because there are multiple js files

--- Exercises/juggle-garden/js/Paddle.js
+++ Exercises/juggle-garden/js/Paddle.js
@@ -1,27 +1,60 @@
 class Paddle {
     constructor(img) {
-        this.width = 60;
-        this.height = 80;
-        this.x = width / 2; // Start the paddle in the center of the canvas
-        this.y = height - this.height / 2;
-        this.speed = 8; // Adjust the speed as needed
-        this.image = img; // Assign the paddle image
+        // Constructor code here
     }
 
     move() {
-        if (keyIsDown(65)) { // Check if 'A' key is pressed
-            this.x -= this.speed;
-        }
-        if (keyIsDown(68)) { // Check if 'D' key is pressed
-            this.x += this.speed;
-        }
-
-        // Limit the paddle's x position to stay within the canvas width
-        this.x = constrain(this.x, this.width / 2, width - this.width / 2);
+        // Move function code here
     }
 
     display() {
-        imageMode(CENTER);
-        image(this.image, this.x, this.y, this.width, this.height); // Display the image
+        // Display function code here
+    }
+}
+
+// Rest of your code
+let gameState = "playing"; // Initial game state
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    paddle = new Paddle(paddleImage);
+    let ball = new Ball(random(0, width), random(-400, -100), ballImage);
+
+    for (let i = 0; i < numBalls; i++) {
+        let x = random(0, width);
+        let y = random(-400, -100);
+        let ball = new Ball(x, y, ballImage);
+        balls.push(ball);
+    }
+}
+
+function draw() {
+    background(25, 25, 112);
+
+    paddle.move();
+    paddle.display();
+
+    if (gameState === "playing") {
+        for (let i = 0; i < balls.length; i++) {
+            let ball = balls[i];
+            if (ball.active) {
+                ball.gravity(gravityForce);
+                ball.move();
+                ball.bounce(paddle, gameState);
+                ball.display();
+            }
+        }
+    } else if (gameState === "gameOver") {
+        // Display your end screen here
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        fill(255);
+        text("You survived!", width / 2, height / 2);
+    }
+}
+
+function keyPressed() {
+    if (keyCode === ESCAPE) {
+        gameState = "gameOver"; // Change the game state when the ESC key is pressed
     }
 }
