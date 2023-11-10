bfd1b32cd44903b9226537eb8d37cb160f35b258
Kessiii <kesvi@icloud.com>
2023-11-09 23:33:47 -0500

    Setting up the frame
    I am creating the 3 states for my prototype to get myself organized.

--- Projects/Project2/Prototype/Star-Crossed-Lovers/js/script.js
+++ Projects/Project2/Prototype/Star-Crossed-Lovers/js/script.js
@@ -11,6 +11,9 @@
 /**
  * Description of preload
 */
+
+let state = 0; //0 for beginning, 1 for game, 2 for end
+
 function preload() {
 
 }
@@ -19,8 +22,14 @@ function preload() {
 /**
  * Description of setup
 */
-function setup() {
 
+function setup() {
+    createCanvas(windowX, windowY);
+  noStroke();
+  textFont("Arial");
+  textSize(24);
+  textAlign(CENTER, CENTER);
+  updateState();
 }
 
 
@@ -30,3 +39,20 @@ function setup() {
 function draw() {
 
 }
+
+function updateState() {
+    background(state === 0 ? "green" : "pink");
+    fill(255);
+    if (state === 0) {
+      text("Insert comic here", width / 2, height / 2);
+    } else if (state === 1) {
+      text("Game is here", width / 2, height / 2);
+    } else {
+      text("End comic here", width / 2, height / 2);
+    }
+  }
+  
+  function mousePressed() {
+    state = (state + 1) % 3; // Toggle between 0, 1, and 2
+    updateState();
+  }
