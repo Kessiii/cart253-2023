96cc056fa70c84a3a69d2e647a085bf02827432f
Kessiii <kesvi@icloud.com>
2023-11-16 23:49:22 -0500

    Trying to change
    After every click I want to change the appearance
    
    This is hard...

--- Exercises/make-some-noise/js/script.js
+++ Exercises/make-some-noise/js/script.js
@@ -9,69 +9,63 @@
 "use strict";
 
 // The balls
-// Set up variables for tracking which ball is being dragged
-let dragging = false;
-let ballBeingDragged;
-// Array to hold ball objects
+let balls = [];
 
+// F-minor
+let notes = [`F3`,`G3`,`Ab4`,`Bb4`,`C4`,`Db4`,`Eb4`,`F4`];
 
-// Ball object constructor
-function Ball(x, y, note) {
- this.x = x;
- this.y = y;
- this.note = note;
- this.radius = 20;
- this.color = color(random(255), random(255), random(255));
-
- // Move the ball
- this.move = function() {
-    this.x += random(-5, 5);
-    this.y += random(-5, 5);
- };
-
- // Check if the ball bounces off the walls
- this.bounce = function() {
-    if (this.x > width - this.radius || this.x < this.radius) {
-      this.x += random(-5, 5);
-    }
-    if (this.y > height - this.radius || this.y < this.radius) {
-      this.y += random(-5, 5);
-    }
- };
-
- // Display the ball
- this.display = function() {
-    fill(this.color);
-    noStroke();
-    ellipse(this.x, this.y, this.radius * 2);
- };
-}
-
-// Notes array
-let notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
-
+// setup()
+//
+// Just creates the canvas.
 function setup() {
- createCanvas(800, 600);
- userStartAudio();
+  createCanvas(600,600);
+
+  userStartAudio();
 }
 
+// draw()
+//
+// Description of draw() goes here.
 function draw() {
- background(0);
+  background(0);
 
- // Iterate over each ball and perform the following actions:
- // move the ball, bounce the ball off the walls, and display the ball.
- for (let i = 0; i < balls.length; i++) {
-    balls[i].move();
-    balls[i].bounce();
-    balls[i].display();
- }
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
 }
 
 function mousePressed() {
- // Create a new ball with random properties
- let note = random(notes);
- let ball = new Ball(mouseX, mouseY, note);
-
- // Add the new ball to the balls array
- balls.push(ball);
+  createBall(mouseX,mouseY);
 }
+
+function createBall(x,y) {
+  let note = random(notes);
+  let ball = new MusicalBall(x,y,note);
+  balls.push(ball);
+}
+
+function createMusicalBall(x, y) {
+      // Create different types of balls based on some condition (for example, random)
+  let note = random(notes);
+
+  // Define appearance properties
+  let ballSize = random(30, 60);
+  let ballColor = {
+    r: random(100, 255),
+    g: random(100, 255),
+    b: random(100, 255),
+  };
+
+  // Create instances of different ball types with appearance properties
+  let ball;
+  if (random() > 0.5) {
+    ball = new SineWaveBall(x, y, note, ballSize, ballColor);
+  } else {
+    ball = new SawtoothWaveBall(x, y, note, ballSize, ballColor);
+  }
+
+  balls.push(ball);
+  }
