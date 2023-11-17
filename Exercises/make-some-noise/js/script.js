69cc6f5b9bd56e1eb454ca4fb19b744f68dca769
Kessiii <kesvi@icloud.com>
2023-11-16 22:51:00 -0500

    Replacing
    Trasnfered the wrong file!
    
    And started coding the script file

--- Exercises/make-some-noise/js/script.js
+++ Exercises/make-some-noise/js/script.js
@@ -8,105 +8,41 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
+// The balls
+let balls = [];
 
-}
+// F-minor
+let notes = [`F3`,`G3`,`Ab4`,`Bb4`,`C4`,`Db4`,`Eb4`,`F4`];
 
-
-let cols, rows;
-let scl = 20;
-let w = 600;
-let h = 400;
-let particles = [];
-let flowfield;
-
+// setup()
+//
+// Just creates the canvas.
 function setup() {
-  createCanvas(w, h);
-  cols = floor(w / scl);
-  rows = floor(h / scl);
-  flowfield = new Array(cols * rows);
+  createCanvas(600,600);
 
-  for (let i = 0; i < 100; i++) {
-    particles[i] = new Particle();
-  }
-  background(150);
+  userStartAudio();
 }
 
+// draw()
+//
+// Description of draw() goes here.
 function draw() {
-  // Calculate and update the flow field
-  let yoff = 0;
-  for (let y = 0; y < rows; y++) {
-    let xoff = 0;
-    for (let x = 0; x < cols; x++) {
-      let index = x + y * cols;
-      let angle = noise(xoff, yoff, frameCount * 0.01) * TWO_PI * 2;
-      let v = p5.Vector.fromAngle(angle);
-      v.setMag(1);
-      flowfield[index] = v;
-      xoff += 0.1;
-      stroke(0, 50);
-      // Uncomment the line below to visualize the flow field vectors
-      // line(x * scl, y * scl, x * scl + v.x * 10, y * scl + v.y * 10);
-    }
-    yoff += 0.1;
-  }
+  background(0);
 
-  // Update and display particles
-  for (let i = 0; i < particles.length; i++) {
-    particles[i].follow(flowfield);
-    particles[i].update();
-    particles[i].edges();
-    particles[i].show();
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
   }
 }
 
-class Particle {
-  constructor() {
-    this.pos = createVector(random(width), random(height));
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-    this.maxspeed = 4;
-    this.prevPos = this.pos.copy();
-  }
+function mousePressed() {
+  createBall(mouseX,mouseY);
+}
 
-  update() {
-    this.vel.add(this.acc);
-    this.vel.limit(this.maxspeed);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-  }
-
-  applyForce(force) {
-    this.acc.add(force);
-  }
-
-  follow(flow) {
-    let x = floor(this.pos.x / scl);
-    let y = floor(this.pos.y / scl);
-    let index = x + y * cols;
-    let force = flow[index];
-    this.applyForce(force);
-  }
-
-  show() {
-    stroke(255, 0, 0, 150);
-    strokeWeight(2);
-    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
-    this.updatePrev();
-  }
-
-  updatePrev() {
-    this.prevPos.x = this.pos.x;
-    this.prevPos.y = this.pos.y;
-  }
-
-  edges() {
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
-  }
+function createBall(x,y) {
+  let note = random(notes);
+  let ball = new Ball(x,y,note);
+  balls.push(ball);
 }
