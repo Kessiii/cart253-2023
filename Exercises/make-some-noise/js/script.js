eddaca54563a4c6e04fec4b0dc1c1cbf67a45719
Kessiii <kesvi@icloud.com>
2023-11-16 23:17:15 -0500

    Dragging
    Trying to make the balls be draggable by the user.

--- Exercises/make-some-noise/js/script.js
+++ Exercises/make-some-noise/js/script.js
@@ -9,40 +9,58 @@
 "use strict";
 
 // The balls
-let balls = [];
+// Set up variables for tracking which ball is being dragged
+let dragging = false;
+let ballBeingDragged;
+let balls = []
 
-// F-minor
-let notes = [`F3`,`G3`,`Ab4`,`Bb4`,`C4`,`Db4`,`Eb4`,`F4`];
-
-// setup()
-//
-// Just creates the canvas.
 function setup() {
-  createCanvas(600,600);
+ createCanvas(600,600);
 
-  userStartAudio();
+ userStartAudio();
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
-  background(0);
+ background(0);
 
-  for (let i = 0; i < balls.length; i++) {
+ for (let i = 0; i < balls.length; i++) {
     let ball = balls[i];
     ball.move();
     ball.bounce();
     ball.display();
-  }
+ }
 }
 
 function mousePressed() {
-  createBall(mouseX,mouseY);
+ // Check if a ball is being clicked
+ for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    let d = dist(mouseX, mouseY, ball.x, ball.y);
+    if (d < ball.r) {
+      dragging = true;
+      ballBeingDragged = ball;
+      break;
+    }
+ }
+}
+
+function mouseDragged() {
+ if (dragging) {
+    ballBeingDragged.x = mouseX;
+    ballBeingDragged.y = mouseY;
+    
+    // Change the sound based on the mouseY position
+    let newNoteIndex = constrain(floor(map(mouseY, 0, height, 0, notes.length-1)), 0, notes.length-1);
+    ballBeingDragged.note = notes[newNoteIndex];
+ }
+}
+
+function mouseReleased() {
+ dragging = false;
 }
 
 function createBall(x,y) {
-  let note = random(notes);
-  let ball = new Ball(x,y,note);
-  balls.push(ball);
+ let note = random(notes);
+ let ball = new Ball(x,y,note);
+ balls.push(ball);
 }
