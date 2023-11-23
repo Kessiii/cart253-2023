808ac5df5b2f6f8f9e1928b5aa6ff0ff1835e7f7
Kessiii <kesvi@icloud.com>
2023-11-23 16:55:02 -0500

    Adding Reflection Exercise

--- Projects/Project2/Prototype/Star-Crossed-Lovers/js/script.js
+++ Projects/Project2/Prototype/Star-Crossed-Lovers/js/script.js
@@ -76,20 +76,48 @@ var scl = 20;
 var w = 1400;
 var h = 1000;
 
+var flying = 0;
 
+var terrain = [];
 
 function mountainSetup() {
+  createCanvas(900, 900, WEBGL);
+  cols = w / scl;
+  rows = h / scl;
 
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
+    for (var y = 0; y < rows; y++) {
+      terrain[x][y] = 0; //specifiying a default for now
+    }
+  }
 }
 
 function mountains() {
   background(0);
 
-  for (var x = 0; x < close; x++) {
-    for (var y = 0 ; y < rows; y++) {
-      
-
+  flying -= 0.1;
+  var yoff = flying;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
+      off += 0.2;
     }
+    yoff += 0.2;
   }
   
+  background(0);
+  translate(0, 50);
+  rotateX(PI / 3);
+  fill(200,200,200, 150);
+  translate(-w / 2, -h / 2);
+  for (var y = 0; y < rows - 1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (var x = 0; x < cols; x++) {
+      vertex(x * scl, y* scl, terrain[x][y]);
+      vertex(x * scl, (y + 1)* scl,terrain[x][y + 1]);
+    }
+    endShape()
+  }
 }
