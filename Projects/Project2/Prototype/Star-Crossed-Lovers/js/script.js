f0f11afa2fb2b4fef3e7b99a8b69c85ef052d7c7
Kessiii <kesvi@icloud.com>
2023-11-28 20:00:52 -0500

    troubleshooting
    -Figured out why the Mountains were not generating in my code.
    -Played with Ambient Lighting and Material to create a more in dept plane.
    -Played with the colours to create a more cohesive terrain.

--- Projects/Project2/Prototype/Star-Crossed-Lovers/js/script.js
+++ Projects/Project2/Prototype/Star-Crossed-Lovers/js/script.js
@@ -11,18 +11,28 @@
  * Description of preload
 */
 
-let state = `title`; //0 for beginning, 1 for game, 2 for end
+let state = `game`; //0 for beginning, 1 for game, 2 for end
+let _text;
 
+let cols, rows;
+let scl = 20;
+let w = 1400;
+let h = 1000;
 
-function preload() {
+let flying = 0;
 
-}
-
+let terrain = [];
 
 function setup() {
-
   mountainSetup();
-  mountains();
+
+  _text = createGraphics(window.innerWidth - 4, window.innerHeight - 4);
+  _text.textFont('Helvetica');
+  _text.textAlign(CENTER);
+  _text.textSize(50);
+  _text.fill(255);
+  _text.noStroke();
+  _text.text('Comic Starts Here', width * 0.5, height * 0.5);
 }
 
 
@@ -40,16 +50,17 @@ function draw() {
 
 function title() {
   push();
-  textSize(20);
-  fill(255,150,150);
-  textAlign(CENTER,CENTER);
-  text(`Comic starts here`,width/2,height/2);
+  _text.clear();
+  background(217);
+  noStroke();
+  _text.text("New Text " + Math.floor(Math.random() * 10), width/2, height/2);
+  texture(_text);
+  plane(window.innerWidth - 4, window.innerHeight - 4);
   pop();
 }
 
 function game() {
-  mountains();
-  mountainSetup();
+  mountainDraw();
 }
 
 function end() {
@@ -66,60 +77,57 @@ function mousePressed() {
     state = `game`;
   }
   else if (state === `game`) {
-      state = `end`;
+    state = `end`;
   }
   else if (state === `end`) {
     state = `title`;
   }
 }
 
-var close, rows;
-var scl = 20;
-var w = 1400;
-var h = 1000;
-
-var flying = 0;
-
-var terrain = [];
-
 function mountainSetup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
- var cols = w / scl;
- var rows = h / scl;
+  cols = w / scl;
+  rows = h / scl;
 
   for (var x = 0; x < cols; x++) {
     terrain[x] = [];
     for (var y = 0; y < rows; y++) {
-      terrain[x][y] = 0; //specifiying a default for now
+      terrain[x][y] = 0; //specify a default value for now
     }
   }
 }
 
-function mountains() {
-  background(0);
-
+function mountainDraw() {
   flying -= 0.1;
   var yoff = flying;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
       terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
-      off += 0.2;
+      xoff += 0.2;
     }
     yoff += 0.2;
   }
-  
-  background(0);
+
+  let locX = mouseX - width / 2;
+  let locY = mouseY - height / 1.5 ;
+  pointLight(255, 255, 255, locX, locY, 5);
+
+  background(0, 0, 255);
   translate(0, 50);
   rotateX(PI / 3);
-  fill(200,200,200, 150);
+
+  // fill(200, 200, 200);
   translate(-w / 2, -h / 2);
   for (var y = 0; y < rows - 1; y++) {
     beginShape(TRIANGLE_STRIP);
     for (var x = 0; x < cols; x++) {
-      vertex(x * scl, y* scl, terrain[x][y]);
-      vertex(x * scl, (y + 1)* scl,terrain[x][y + 1]);
+      vertex(x * scl, y * scl, terrain[x][y]);
+      vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
     }
-    endShape()
+    ambientLight(35 / rows * y, 0, 0);
+    ambientMaterial(255 / rows * y, 20, 200);
+
+    endShape();
   }
 }
